5f1491cb52095f460b05a4aa18e7d20b33d029ed
hhuziuk <huziukwork@gmail.com>
2024-10-27 19:35:42 +0100

    updated code

--- lab-c/index.js
+++ lab-c/index.js
@@ -1,42 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let marker;
     let map = L.map('map').setView([50.45, 25.21], 18);
+    let marker;
 
     L.tileLayer.provider('Esri.WorldImagery').addTo(map);
 
-    const settings = {
+    const customMarkerIcon = L.icon({
         iconUrl: 'https://data.chpic.su/stickers/d/duraktndrch/duraktndrch_023.webp',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
-    }
+    });
 
-    let customMarkerIcon = L.icon(settings);
-
-    document.getElementById("getLocation").addEventListener("click", function () {
+    document.getElementById("getLocation").addEventListener("click", () => {
         if (!navigator.geolocation) {
             console.log("No geolocation.");
             return;
         }
 
-        navigator.geolocation.getCurrentPosition(position => {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-            map.setView([lat, lon]);
-            if (marker) { map.removeLayer(marker); }
-            marker = L.marker([lat, lon]).addTo(map).bindPopup("You are here!").openPopup();
-        }, positionError => { console.error(positionError); });
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude: lat, longitude: lon } = position.coords;
+                map.setView([lat, lon]);
+
+                if (marker) {
+                    map.removeLayer(marker);
+                }
+                marker = L.marker([lat, lon], { icon: customMarkerIcon }).addTo(map)
+                    .bindPopup("You are here!")
+                    .openPopup();
+            },
+            (error) => console.error(error)
+        );
     });
 
-    document.getElementById("saveButton").addEventListener("click", function () {
-        leafletImage(map, function (err, canvas) {
+    document.getElementById("saveButton").addEventListener("click", () => {
+        leafletImage(map, (err, canvas) => {
             if (err) {
                 console.error(err);
                 return;
             }
-            let savedMapCanvas = document.getElementById('savedMapCanvas');
-            let rasterContext = savedMapCanvas.getContext("2d");
+            const savedMapCanvas = document.getElementById('savedMapCanvas');
+            const rasterContext = savedMapCanvas.getContext("2d");
             rasterContext.clearRect(0, 0, savedMapCanvas.width, savedMapCanvas.height);
             rasterContext.drawImage(canvas, 0, 0, savedMapCanvas.width, savedMapCanvas.height);
             generatePuzzlePieces(savedMapCanvas);
@@ -44,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    function generatePuzzlePieces(rasterMap) {
+    const generatePuzzlePieces = (rasterMap) => {
         const puzzleContainer = document.getElementById('puzzleContainer');
         puzzleContainer.innerHTML = '';
 
@@ -53,13 +58,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
         for (let row = 0; row < 4; row++) {
             for (let col = 0; col < 4; col++) {
-                let pieceCanvas = document.createElement('canvas');
+                const pieceCanvas = document.createElement('canvas');
                 pieceCanvas.width = pieceSize;
                 pieceCanvas.height = pieceSize;
-                let context = pieceCanvas.getContext('2d');
+                const context = pieceCanvas.getContext('2d');
                 context.drawImage(rasterMap, col * pieceSize, row * pieceSize, pieceSize, pieceSize, 0, 0, pieceSize, pieceSize);
-
-                let piece = document.createElement('div');
+                const piece = document.createElement('div');
                 piece.classList.add('puzzle-piece');
                 piece.appendChild(pieceCanvas);
                 piece.setAttribute('draggable', true);
@@ -70,34 +74,29 @@ document.addEventListener('DOMContentLoaded', () => {
                 pieces.push(piece);
             }
         }
-        pieces = shuffleArray(pieces);
-        pieces.forEach(piece => puzzleContainer.appendChild(piece));
-    }
+        shuffleArray(pieces).forEach(piece => puzzleContainer.appendChild(piece));
+    };
 
-    function generateDropzones() {
-        const dropNzoneContainer = document.getElementById('dropNzoneContainer');
-        dropNzoneContainer.innerHTML = '';
+    const generateDropzones = () => {
+        const dropzoneContainer = document.getElementById('dropzoneContainer');
+        dropzoneContainer.innerHTML = '';
         for (let i = 0; i < 16; i++) {
-            let dropzone = document.createElement('div');
+            const dropzone = document.createElement('div');
             dropzone.classList.add('dropzone');
             dropzone.addEventListener('dragover', handleDragOver);
             dropzone.addEventListener('drop', handleDrop);
-            dropNzoneContainer.appendChild(dropzone);
+            dropzoneContainer.appendChild(dropzone);
         }
-    }
+    };
 
     const handleDragStart = (event) => {
         event.dataTransfer.setData('text/plain', event.target.id);
         event.target.classList.add('dragging');
-    }
+    };
 
-    const handleDragEnd= (event) => {
-        event.target.classList.remove('dragging');
-    }
+    const handleDragEnd = (event) => {event.target.classList.remove('dragging');};
 
-    const handleDragOver = (event) => {
-        event.preventDefault();
-    }
+    const handleDragOver = (event) => { event.preventDefault();};
 
     const handleDrop = (event) => {
         event.preventDefault();
@@ -105,12 +104,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (event.target.classList.contains('dropzone') && event.target.children.length === 0) {
             event.target.appendChild(draggedElement);
-
-            if (isPuzzleSolved()) {
-                showNotification('Puzzle completed successfully!');
-            }
+            if (isPuzzleSolved()) { showNotification('Puzzle completed successfully!');}
         }
-    }
+    };
 
     const isPuzzleSolved = () => {
         const dropzones = document.querySelectorAll('.dropzone');
@@ -118,13 +114,13 @@ document.addEventListener('DOMContentLoaded', () => {
             const piece = dropzone.firstElementChild;
             return piece && piece.dataset.position === indexToPosition(index);
         });
-    }
+    };
 
     const indexToPosition = (index) => {
         const row = Math.floor(index / 4);
         const col = index % 4;
         return `${row}-${col}`;
-    }
+    };
 
     const shuffleArray = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
@@ -132,17 +128,16 @@ document.addEventListener('DOMContentLoaded', () => {
             [array[i], array[j]] = [array[j], array[i]];
         }
         return array;
-    }
+    };
 
     const showNotification = (message) => {
-        alert("you did everything good:)")
+        alert("Great job on the puzzle!");
         if (!("Notification" in window)) {
-            alert("yur google chroma can not work with it lol");
+            alert("Your browser does not support notifications.");
             return;
         }
-
         Notification.requestPermission().then(permission => {
-            if (permission === "granted") { new Notification("play with map", { body: message }); }
+            if (permission === "granted") { new Notification("Map Puzzle Game", { body: message }); }
         });
-    }
+    };
 });
