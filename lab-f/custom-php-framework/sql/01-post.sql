563c259b2b9aae03283174cfcef8c2aa2b1792d9
HzHeorhii1 <65177840+HzHeorhii1@users.noreply.github.com>
2024-11-26 23:45:31 +0100

    added lab-f

---
+++ lab-f/custom-php-framework/sql/01-post.sql
@@ -0,0 +1,15 @@
+create table post
+(
+    id      integer not null
+        constraint post_pk
+            primary key autoincrement,
+    subject text not null,
+    content text not null
+);
+
+CREATE TABLE book (
+                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      title TEXT NOT NULL,
+                      author TEXT NOT NULL,
+                      description TEXT
+);
